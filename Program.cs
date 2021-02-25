3d98a62fbda28b94b39168f3cb3d8ae7d057f592
Medic Gordus <gordusthemedic@outlook.com>
2021-02-25 14:59:40 -0800

    Initial checkin

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vanwall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
